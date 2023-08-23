c5351b3dd7adfce4725f11ce7bc18ea3006d3cfb
Santiago Feijoo <santiago.feijoo96@gmail.com>
2023-08-23 00:18:25 -0300

    Refactor del sistema Legacy del input del usuario. Se usa el nuevo sistema de control de Input. Se dispara el evento OnInteract en GameInput cuando se desea interactuar con un item. El listener, en este caso el playerController, logra escuchar el evento y actuar en consecuencia

---
+++ Assets/src/GameInput.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameInput : MonoBehaviour
+{
+    // LEGACY INPUT MANAGER
+    // en cada frame se verifica qué tecla se presionó y se acciona
+    // Update is called once per frame
+
+    /**
+     * GetKey sirve para saber si una tecla esta siendo presionada a lo largo del tiempo, como por ejemplo, cuando un personaje se mueve por el mapa
+     * GeyKeyDown sirve para detectar si se presionó una tecla en un frame, como por ejemplo, saltar.
+
+        Asi queda bien explicito que devuelve un vector unitario
+        public Vector2 GetMovementNormalizedVector() 
+{
+        // Movimientos en el plano XZ. No es necesario un vector 3D para capturar el movimiento
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+
+        inputVector = inputVector.normalized; // esto es para que sea siempre un vector unitario. Si yo presiono 2 teclas, la magnitud debe seguir siendo 1.
+
+        return inputVector;
+        }
+        */
+
+    /* NEW INPUT SYSTEM
+     * 
+     *  para el tema del movimiento, es razonable chequearlo frame a frame, pero para las demas cosas como las interacciones, 
+     *  como no son cosas que van a estar sucediendo constantemente, se utilizan los Eventos. Como es de costumbre,
+     *  a la hora de suscribirme a un evento, le paso una función callback para que cuando suceda ese evento, llamen a esa función.
+     *  
+     *  Cuando se dispara el evento:
+     *      OnInteractAction?.Invoke(this, EventArgs.Empty);
+     *  - El primer argumento es la referencia a "quien disparó el evento"
+     *  - El segundo es un Array de datos que pueden llegar a necesitar los suscriptores.
+     *  - El tema del parentesis es similar al de Node Js. Si no es null, invoca a la función con esos argumentos.
+     *  Es necesario tener suscriptores a la hora de disparar el evento.
+     */
+
+
+    private PlayerInputActions inputActions;
+    public event EventHandler OnInteractAction;
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Enable(); // Se encuentra en Actions maps dentro de la configuración del PlayerInputActions
+
+        inputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+        Debug.Log("E pressed");
+    }
+
+    public Vector2 GetMovementNormalizedVector()
+    {
+        Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
+        return inputVector.normalized; // esta normalizacion se puede hacer tambien desde PlayerInputActions, en una sección llamada "Processors"
+    }
+}
--- Assets/src/PlayerController.cs
+++ Assets/src/PlayerController.cs
@@ -8,65 +8,155 @@ public class PlayerController : MonoBehaviour
     private float movementSpeed = 5f;
     [SerializeField]
     private float rotationSpeed = 10f;
+        
+    
     private bool isWalking;
-   
+    private Vector3 lastMovementDirection;
 
-    // LEGACY INPUT MANAGER
-    // en cada frame se verifica qué tecla se presionó y se acciona
-    // Update is called once per frame
+
+    private const float RADIUS = .7f; // el scale de la esfera del cuerpo dividido 2.
+    private const float PLAYER_HEIGHT = 2f; // lo mande a ojo, es lo de menos igual
+    private const float INTERACT_DISTANCE = 2f;
+    [SerializeField]
+    private GameInput gameInput;
+
+
+
+    /*
+       1) La multiplicación del Time.deltaTime se hace para que la velocidad ya no dependa de la cantidad de fps. Recordemos que Update se ejecuta en cada frame
+         Por lo tanto si tengo muchos mas fps, naturalmente la velocidad va a ser mayor. Pero si lo multiplico por un infinitesimal de tiempo, ese numero se mantiene
+        ya que si aumento los fps, el delta es aun más pequeño
+       
+        2) Antes de moverme directamente (asignacion a transform.position) debo verificar si estoy colisionando con algo, sino lo voy a traspasar (Collisions)
+     Physics.Raycast, es como un laser que apunta en cierta direccion y te avisa si colisionas con otro objeto. Sale desde el centro del objeto origen, asi que si en la
+    dirección del centro no detecta colisión, el jugador camina, no importa si con el brazo o algo asi termine chocando con algo.
+    Para evitar este problema, hay otros tipos de lasers que se pueden castear, con diferentes formas, como BoxCast, CapsuleCast o SphereCast.
+
+    Physics.CapsuleCast(capsuleBottom, CapsuleTop, radius, 
+    capsuleBottom es la base del jugador, esta en el suelo.
+    capsule Top es la altura maxima que seria la cabeza
+
+     */
+
 
     /**
-     * GetKey sirve para saber si una tecla esta siendo presionada a lo largo del tiempo, como por ejemplo, cuando un personaje se mueve por el mapa
-     * GeyKeyDown sirve para detectar si se presionó una tecla en un frame, como por ejemplo, saltar.
+     * ROTACIÓN
+     * Para manejar la rotación hacia la dirección donde me muevo, hay varias maneras
+     * 1 - transform.rotation
+     * 2 - eulerAngles
+     * 3 - LookAt: rota pero dando la espalda. Si me muevo de forma circular, estaría siempre mirando hacia el centro y no hacia la dirección tangente
+               transform.LookAt(rotationSpeed * Time.deltaTime * move);
+     * 4 - transform.forward
+           transform.forward = move; si hago la rotación así, se logra pero de forma directa, no hay un suave movimiento hacia la rotación objetivo
      */
-    private void Update()
+    private void Start()
     {
-        // Movimientos en el plano XZ. No es necesario un vector 3D para capturar el movimiento
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
+        gameInput.OnInteractAction += GameInput_OnInteractAction; //Listener del evento
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        /* 1)
+         * Con un raycast alcanza ya que no estoy pensando en no chocarme con cosas, simplemente saber si tengo algo adelante para interactuar, y qué objeto
+         * Hay muchas firmas de Raycast, algunas devuelven un booleano, que sirve para saber si hay algo adelante o no.
+         * Como en este caso me interesa interactuar, necesito obtener la referencia del objeto. Para ello, utilizo la firma con el parametro "out RaycastHit info".
+         * Esta variable referencia al objeto con el que colisiona el jugador. (side effect)
+         * 
+         * 2) Si yo me quedo parado, no me estoy moviendo (duh). Eso quiere decir que el vector move será nulo y por lo tanto no voy a mandar un Raycast a ningun lugar, por lo que
+         * por más que tenga un objeto en frente, no voy a detectarlo.
+         * Para solucionar esto, cuando guardarme una referencia de la ultima dirección a la que me moví. De ahi nace lastMovementDirection
+         * 
+         * 3) En este juego solamente voy a interactuar con clearCounters, pero en caso de tener un monton de clases distintas para interactuar, como abrir puertar,
+           interactuar con npcs, apretar botones y demás, hay otro enfoque mucho mejor que una lluvia de ifs
+        */
+        Debug.Log("Intento de interacción detectada");
+        Vector2 inputVector = gameInput.GetMovementNormalizedVector();
+        Vector3 move = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (move != Vector3.zero)
         {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
+            lastMovementDirection = move;
         }
 
-        inputVector = inputVector.normalized; // esto es para que sea siempre un vector unitario. Si yo presiono 2 teclas, la magnitud debe seguir siendo 1.
+        if (Physics.Raycast(transform.position, lastMovementDirection, out RaycastHit hit, INTERACT_DISTANCE)) // Hay algo adelante?
+        {
+
+            if (hit.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementNormalizedVector();
 
         //modifico la posición del jugador. Como el jugador se mueve en un mapa de 3 dimensiones, es necesario hacer el casteo. Asi tengo separado el vector input, del movimiento en sí.
         Vector3 move = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position +=  (Time.deltaTime * movementSpeed * move);
-        // esta multiplicación se hace para que la velocidad ya no dependa de la cantidad de fps. Recordemos que Update se ejecuta en cada frame
-        // Por lo tanto si tengo muchos mas fps, naturalmente la velocidad va a ser mayor. Pero si lo multiplico por un infinitesimal de tiempo, ese numero se mantiene
-        // ya que si aumento los fps, el delta es aun más pequeño
 
 
+        float moveDistance = Time.deltaTime * movementSpeed;
 
-        /**ROTACIÓN
-         * Para manejar la rotación hacia la dirección donde me muevo, hay varias maneras
-         * 1 - transform.rotation
-         * 2 - eulerAngles
-         * 3 - LookAt: rota pero dando la espalda. Si me muevo de forma circular, estaría siempre mirando hacia el centro y no hacia la dirección tangente
-                   transform.LookAt(rotationSpeed * Time.deltaTime * move);
-         * 4 - transform.forward
-         */
+        bool collisionDetected = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PLAYER_HEIGHT, RADIUS, move, moveDistance);
 
-        // transform.forward = move; si hago la rotación así, se logra pero de forma directa, no hay un suave movimiento hacia la rotación objetivo
+        // Si me muevo en diagonal al chocar contra un obstaculo, me fijo si es posible moverme en la dirección paralela.
+        if (collisionDetected)
+        {
+            //intento moverme en X 
+            Vector3 moveX = new Vector3(move.x, 0, 0).normalized; // para evitar moverme a una velocidad diferente. Siempre debe ser un vector unitario
+            collisionDetected = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PLAYER_HEIGHT, RADIUS, moveX, moveDistance);
+            if (!collisionDetected)
+            {
+                move = moveX;
+            }
+            else
+            {
+                //ahora intento moverme en el plano z
+                Vector3 moveZ = new Vector3(0, 0, move.z).normalized;
+                collisionDetected = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PLAYER_HEIGHT, RADIUS, moveZ, moveDistance);
+                if (!collisionDetected)
+                {
+                    move = moveZ;
+                }
+            }
+        }
+
+        if (!collisionDetected)
+        {
+            transform.position += (moveDistance * move);
+        }
         isWalking = move != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, move, Time.deltaTime * rotationSpeed);
-
-
     }
 
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementNormalizedVector();
+        Vector3 move = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (move != Vector3.zero)
+        {
+            lastMovementDirection = move;
+        }
+
+        if (Physics.Raycast(transform.position, lastMovementDirection, out RaycastHit hit, INTERACT_DISTANCE)) // Hay algo adelante?
+        {
+
+            if (hit.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter))
+            {
+                //clearCounter.Interact();
+            }
+
+        }
+    }
     public bool IsWalking()
     {
         return isWalking;
